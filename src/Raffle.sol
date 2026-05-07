15debd99a80d42da7381f14fa62841d763cb25da
Shahbaz <sc@shahbazc.com>
2026-05-07 18:03:26 +0100

    initial commit

---
+++ src/Raffle.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/**
+ * @author  0xGaladhrim
+ * @title   A sample Raffle contract
+ * @dev     Implements Chainlink VRFv2.5
+ * @notice  This contract is for creating a sample raffle
+ */
+contract Raffle {
+    function enterRaffle() public {}
+
+    function pickWinner() public {}
+}
