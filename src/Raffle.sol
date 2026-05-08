82c620c506df19435c7304f623d5c58f30b40c22
Shahbaz <sc@shahbazc.com>
2026-05-08 16:14:15 +0100

    added s_players array and EnteredRaffle() event

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -8,17 +8,28 @@ pragma solidity ^0.8.18;
  * @notice  This contract is for creating a sample raffle
  */
 contract Raffle {
+    /* Events */
+    event EnteredRaffle(address indexed player);
+
     /* Errors */
     error Raffle__NotEnoughEthSent();
 
     uint256 private immutable ENTRANCE_FEE;
+    address payable[] private s_players;
 
     constructor(uint256 entranceFee) {
         ENTRANCE_FEE = entranceFee;
     }
 
     function enterRaffle() external payable {
+        // validate to ensure user value is not less than required entrance fee
         if (msg.value < i_entranceFee) revert Raffle__NotEnoughEthSent();
+
+        // add user address to s_players to track registration
+        s_players.push(payable(msg.sender));
+
+        // emit event
+        emit EnteredRaffle(msg.sender);
     }
 
     function pickWinner() public {}
