6efb524897dac44f1c0ce32958d84ab190df0f08
Shahbaz <sc@shahbazc.com>
2026-05-11 16:57:40 +0100

    implement struct values for pickWinner() function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
  * @author  0xGaladhrim
@@ -16,20 +17,35 @@ contract Raffle is VRFConsumerBaseV2Plus {
     /* Errors */
     error Raffle__NotEnoughEthSent();
 
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable ENTRANCE_FEE;
     uint256 private immutable INTERVAL; // @dev duration of the lottery in seconds
+    bytes32 private immutable KEY_HASH;
+    uint256 private immutable SUBSCRIPTION_ID;
+    uint32 private immutable CALLBACK_GAS_LIMIT;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
 
-    constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator) VRFConsumerBaseV2Plus(vrfCoordinator) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         ENTRANCE_FEE = entranceFee;
-        INTERVAL = inteval;
+        INTERVAL = interval;
         s_lastTimeStamp = block.timestamp;
+        KEY_HASH = gasLane;
+        SUBSCRIPTION_ID = subscriptionId;
+        CALLBACK_GAS_LIMIT = callbackGasLimit;
     }
 
     function enterRaffle() external payable {
         // validate to ensure user value is not less than required entrance fee
-        if (msg.value < i_entranceFee) revert Raffle__NotEnoughEthSent();
+        if (msg.value < ENTRANCE_FEE) revert Raffle__NotEnoughEthSent();
 
         // add user address to s_players to track registration
         s_players.push(payable(msg.sender));
@@ -42,16 +58,14 @@ contract Raffle is VRFConsumerBaseV2Plus {
         // check to see if enough times has passed
         if ((block.timestamp - s_lastTimeStamp) < INTERVAL) revert();
 
-        requestId = s_vrfCoordinator.requestRandomWords(
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
             VRFV2PlusClient.RandomWordsRequest({
-                keyHash: keyHash,
-                subId: s_subscriptionId,
-                requestConfirmations: requestConfirmations,
-                callbackGasLimit: callbackGasLimit,
-                numWords: numWords,
-                extraArgs: VRFV2PlusClient._argsToBytes(
-                    VRFV2PlusClient.ExtraArgsV1({nativePayment: enableNativePayment})
-                )
+                keyHash: KEY_HASH,
+                subId: SUBSCRIPTION_ID,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: CALLBACK_GAS_LIMIT,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
             })
         );
     }
