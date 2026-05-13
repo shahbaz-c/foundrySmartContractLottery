b346420cf5901efa7a84682dbe2cb291ae071115
Shahbaz <sc@shahbazc.com>
2026-05-13 14:01:58 +0100

    added to deployContract() function

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -3,16 +3,28 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
     function run() external {
         deployContract();
     }
 
-    function deployContract()
-        internal
-        returns (
-            Raffle /*, HelperConfig */
-        )
-    {}
+    function deployContract() internal returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        return (raffle, helperConfig);
+    }
 }
